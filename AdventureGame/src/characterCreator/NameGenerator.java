b80377a4e6f3a8ffe7b267dd666adc8fc01caf22
ErikSionov <ErikSionov@gmail.com>
2021-08-01 05:36:19 +0300

    added prefixes and suffixes

--- AdventureGame/src/characterCreator/NameGenerator.java
+++ AdventureGame/src/characterCreator/NameGenerator.java
@@ -3,35 +3,35 @@ package characterCreator;
 import java.util.Random;
 
 public class NameGenerator {
-	
-	 public static void main(String[] args) {
-		
-		 String randomName = generateRandomName();
-		
-		 System.out.println("randomly generated name = " + randomName);
-		 
+
+	public static void main(String[] args) {
+
+		String randomName = generateRandomName();
+
+		System.out.println("randomly generated name = " + randomName);
+
 	}
 
 	private static String generateRandomName() {
 
 		String name = "";
-		
+
 		Random random = new Random();
-		
-		String[] preffix = {"Da"};
-		String[] mid = {"ni"};
-		String[] suffix = {"el"};
-		
-		if(random.nextInt(11) >= 4) {
-			
-			name = preffix[random.nextInt(preffix.length)] + suffix[random.nextInt(suffix.length)];
-			
-		}else {
-			
-			name = preffix[random.nextInt(preffix.length)] + mid[random.nextInt(mid.length)] + suffix[random.nextInt(suffix.length)];
+
+		String[] prefix = {"Da", "Ra", "Na", "Li", "La", "Ma", "So", "Sha", "Ro", "Yo", "A"};
+		String[] mid = {"ni", "fa", "ha", "ta", "cha"};
+		String[] suffix = {"el", "na", "sha", "m", "tam", "ron", "ha", "shi", "ya", "r"};
+
+		if (random.nextInt(11) >= 4) {
+
+			name = prefix[random.nextInt(prefix.length)] + suffix[random.nextInt(suffix.length)];
+
+		} else {
+
+			name = prefix[random.nextInt(prefix.length)] + mid[random.nextInt(mid.length)] + suffix[random.nextInt(suffix.length)];
 		}
-		
+
 		return name;
 	}
-	
+
 }
